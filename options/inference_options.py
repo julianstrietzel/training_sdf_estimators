e435a6a3affdccff9413315dfea49a7ab6330233
Jerry4201 <julian.strietzel@tum.de>
2023-12-03 12:11:42 +0100

    :sparkles: InferenceOptions

--- options/inference_options.py
+++ options/inference_options.py
@@ -1,7 +1,7 @@
 from .base_options import BaseOptions
 
 
-class TestOptions(BaseOptions):
+class InferenceOptions(BaseOptions):
     def initialize(self):
         BaseOptions.initialize(self)
         self.parser.add_argument(
